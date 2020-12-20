856beb418a460ecf6b85a2268b0c328790487822
ragingeagle23 <ragingeagle2017@gmail.com>
2020-12-20 04:09:41 -0800

    Added component and features

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <div>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        </div>
         <h1>My Mental State</h1>
         <h2>James D's Portfolio</h2>
-        <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <PortfolioContainer />
       </div>
     );
   }
